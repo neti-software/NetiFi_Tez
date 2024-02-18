b744bfd8126d5bdf614dcba72cfcb504be69e9dc
Seweryn Ostrowski <seweryn.ostrowski@neti-soft.com>
2024-02-18 01:34:37 +0100

    fronted prod build + dockerfile

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,27 +3,33 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import inject from '@rollup/plugin-inject'
 
 export default defineConfig({
-  resolve: {
-    alias: {
-        stream: 'stream-browserify'
-    }
-  },
-  optimizeDeps: {
-      esbuildOptions: {
-          // Node.js global to browser globalThis
-          define: {
-              global: 'globalThis'
-          },
-          // Enable esbuild polyfill plugins
-          plugins: [
-              NodeGlobalsPolyfillPlugin({
-                  buffer: true
-              })
-          ]
-      }
-  }
+    resolve: {
+        alias: {
+            stream: 'stream-browserify'
+        }
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            // Node.js global to browser globalThis
+            define: {
+                global: 'globalThis'
+            },
+            // Enable esbuild polyfill plugins
+            plugins: [
+                NodeGlobalsPolyfillPlugin({
+                    buffer: true
+                })
+            ]
+        }
+    },
+    build: {
+        rollupOptions: {
+            plugins: [inject({ Buffer: ['buffer/', 'Buffer'] })],
+        },
+    },
 })
 
 
